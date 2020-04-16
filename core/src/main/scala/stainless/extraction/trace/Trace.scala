d6489ddf7ed32675ec5acf583c3200734a080679
Dragana Milovancevic <dragana.milovancevic@epfl.ch>
2020-04-16 13:50:31 +0200

    traceInduct implementation

--- core/src/main/scala/stainless/extraction/trace/Trace.scala
+++ core/src/main/scala/stainless/extraction/trace/Trace.scala
@@ -23,120 +23,127 @@ trait Trace extends CachingPhase with SimpleFunctions with IdentitySorts { self 
     override val s: self.s.type = self.s
     override val t: self.t.type = self.t
   }
-                    
+
   override protected def extractFunction(symbols: Symbols, fd: FunDef): t.FunDef = {
     import symbols._
-    var v3 = fd.fullBody
-    val result: FunDef = if(fd.flags.contains(TraceInduct)) {
-                           fd.fullBody match {
-                             case Ensuring(body, pred) => {
-                               body match {
-                                 case Equals(f1, f2) => {
-                                   val proof: Expr = prove(symbols, fd, f1) match {
-                                                       case Some(e) => e
-                                                       case None => prove(symbols, fd, f2) match {
-                                                         case Some(e) => e
-                                                         case None => fd.fullBody
-                                                       }
-                                                     }
-                                   fd.copy(fd.id, fd.tparams, fd.params, fd.returnType, Ensuring(And(proof, body), pred), fd.flags)
-                                 }
-                                 case _ => fd
-                               }
-                             }
-                             case _ => fd
-                           }
-                         }
-                         else fd
-                         
-    if(result.fullBody != fd.fullBody) System.out.println(result.fullBody)
-    
-    identity.transform(result.copy(flags = result.flags filterNot (f => f == TraceInduct)))
-  }
-  
-  private def prove(symbols: Symbols, fd: FunDef, fcall: Expr): Option[Expr] = {
-    fcall match {
-      case FunctionInvocation(id,t,a) => {
-        funInduct(symbols, fd, symbols.functions.filter(elem => elem._2.id == id).head._2)
-      }
-      case _ => None
-    }
-  }
+    var funInv: Option[FunctionInvocation] = None
 
-  private def funInduct(symbols: Symbols, fd: FunDef, fInduct: FunDef): Option[Expr] = {
-    import symbols._
-
-    System.out.println(fInduct.id)
-    inductPattern(symbols, fInduct.fullBody)
-    //Some(inductPattern(fInduct.fullBody))
-
-    None
-  }
-
-  private def inductPattern(symbols: Symbols, expr: Expr): Expr = {
-    import symbols._
-    
-    expr match {
-      case MatchExpr(scr, cases) => {
-        inductPattern(symbols, matchToIfThenElse(expr))
-      }
-      case IfExpr(cond, thenn, elze) => {
-        System.out.println("if")
-        System.out.println("before")
-        System.out.println(expr)
-        System.out.println("after")
-        System.out.println(IfExpr(inductPattern(symbols, cond), inductPattern(symbols, thenn), inductPattern(symbols, elze)).copiedFrom(expr))
-        IfExpr(inductPattern(symbols, cond), inductPattern(symbols, thenn), inductPattern(symbols, elze)).copiedFrom(expr)
-      }
-      case Operator(args, op) => {
-        System.out.println("op")
-        expr
-      }
-      case Let(i, v, b) => {
-        System.out.println("let")
-        expr
-      }
-      case FunctionInvocation(n, t, p) => {
-        System.out.println("functioninv")
-        expr
-      }
-      case _ => {
-        System.out.println("default")
-        expr
-      }
-    }
-  }
-
-  private def containsRecursiveCalls(expr: Expr, fd: FunDef) = {
-    exprOps.functionCallsOf(expr).map((elem: FunctionInvocation) => elem.id).contains(fd.id)
-  }
-  
-  def replaceSymbols(expr: Expr, from: Seq[Variable], to: Seq[Variable], fromf: FunDef, tof: FunDef): Option[Expr] = {
-    val errorVariable = Variable.fresh("errorVariable", BooleanType())
-    def rec(expr: Expr, from: Seq[Variable], to: Seq[Variable], fromf: FunDef, tof: FunDef): Expr = {
-      new SelfTreeTransformer {
-        override def transform(expr: Expr): Expr = expr match {
-          case v: Variable => {
-            if(from.indexOf(v) != -1) to(from.indexOf(v))
-            else errorVariable
-          }
-          case FunctionInvocation(n, t, p) => {
-            val newp = p.map(elem => rec(elem, from, to, fromf, tof))
-            if (newp.filter(elem => elem == errorVariable).size != 0) errorVariable
-            else if(n != fromf.id) FunctionInvocation(n, t, newp)
-            else FunctionInvocation(tof.id, t, newp)
-          }
-          case _ => super.transform(expr)
+    if(fd.flags.filter(elem => elem.name == "traceInduct").size == 1) {
+      fd.flags.filter(elem => elem.name == "traceInduct").head match {
+        case Annotation("traceInduct", fun) => {
+          exprOps.preTraversal {
+            case _ if funInv.isDefined => // do nothing
+            case fi @ FunctionInvocation(tfd, _, args) if symbols.isRecursive(tfd) && (fun.contains(StringLiteral(tfd.name)) || fun.contains(StringLiteral("")))
+            => {
+                  val paramVars = fd.params.map(_.toVariable)
+                  val argCheck = args.forall(paramVars.contains) && args.toSet.size == args.size
+                  if (argCheck) 
+                    funInv = Some(fi)
+                }
+            case _ => 
+          }(fd.fullBody)
         }
-      }.transform(expr)
+      }
     }
-    
-    val res = rec(expr, from, to, fromf, tof)
-    if(res == errorVariable) None
-    else Some(res)
+
+    val result: FunDef = (funInv match {
+      case None => fd
+      case Some(finv) => createTactFun(symbols, fd, finv)
+    })
+
+    System.out.println(result.fullBody)
+    identity.transform(result.copy(flags = result.flags filterNot (f => f == TraceInduct)))    
   }
   
+  private def createTactFun(symbols: Symbols, function: FunDef, finv: FunctionInvocation): FunDef = {
+    import symbols._
+
+    val callee: FunDef = symbols.functions.filter(elem => elem._2.id == finv.id).head._2
+
+    def inductPattern(e: Expr): Expr = {
+      e match {
+        case IfExpr(c, th, el) =>
+          createAnd(Seq(inductPattern(c), IfExpr(c, inductPattern(th), inductPattern(el))))
+        case MatchExpr(scr, cases) =>
+          val scrpat = inductPattern(scr)
+          val casePats = cases.map {
+            case MatchCase(pat, optGuard, rhs) =>
+              val guardPat = optGuard.toSeq.map(inductPattern _)
+              (guardPat, MatchCase(pat, optGuard, inductPattern(rhs)))
+          }
+          val pats = scrpat +: casePats.flatMap(_._1) :+ MatchExpr(scr, casePats.map(_._2))
+          createAnd(pats)
+        case Let(i, v, b) =>
+          createAnd(Seq(inductPattern(v), Let(i, v, inductPattern(b))))
+        case FunctionInvocation(tfd, _, args) =>
+          val argPattern = createAnd(args.map(inductPattern))
+          if (tfd == callee.id) { // self recursive call ?
+            // create a tactFun invocation to mimic the recursion pattern
+            val paramVars = function.params.map(_.toVariable)
+            val paramIndex = paramVars.zipWithIndex.toMap
+            val framePositions = finv.args.zipWithIndex.collect {
+              case (v: Variable, i) if paramVars.contains(v) => (v, i)
+            }.toMap
+            val footprint = paramVars.filterNot(framePositions.keySet.contains)
+            val indexedFootprint = footprint.map { a => paramIndex(a) -> a }.toMap
+            // create a tactFun invocation to mimic the recursion pattern
+            val indexedArgs = framePositions.map {
+              case (f, i) => paramIndex(f) -> finv.args(i)
+            }.toMap ++ indexedFootprint
+            val recArgs = (0 until indexedArgs.size).map(indexedArgs)
+            val recCall = FunctionInvocation(function.id, function.typeArgs, recArgs)
+
+            createAnd(Seq(argPattern, recCall))
+          } else {
+            argPattern
+          }
+        case Operator(args, op) =>
+          // conjoin all the expressions and return them
+          createAnd(args.map(inductPattern))
+      }
+    }
+
+    val argsMap = callee.params.map(_.toVariable).zip(finv.args).toMap
+    val tparamMap = callee.typeArgs.zip(finv.tfd.tps).toMap
+    val inlinedBody = typeOps.instantiateType(replaceFromIDs(argsMap, callee.body.get), tparamMap)
+    val inductScheme = inductPattern(inlinedBody)
+ 
+    // body, pre and post for the tactFun
+    val body = createAnd(Seq(inductScheme, function.fullBody))
+    val precondition = function.precondition
+    val postcondition = function.postcondition
+ 
+    val bodyPre = exprOps.withPrecondition(body, precondition)
+    val bodyPost = exprOps.withPostcondition(bodyPre,postcondition)
+
+    function.copy(function.id, function.tparams, function.params, function.returnType, bodyPost, function.flags)
+  }
+ 
+
+  private def replaceFromIDs(substs: Map[Variable, Expr], expr: Expr) : Expr = {
+    exprOps.postMap({
+      case a: Variable => substs.get(a)
+      case _ => None
+    })(expr)
+  }
+
+  private def createAnd(exprs: Seq[Expr]): Expr = {
+    // pull ands if any in the subexpression to the top
+    val newExprs = exprs.flatMap {
+      case BooleanLiteral(true) => Seq()
+      case And(args) => args
+      case e => Seq(e)
+    }
+    //val newExprs = exprs.filterNot(conj => conj == tru)
+    newExprs match {
+      case Seq()  => BooleanLiteral(true)
+      case Seq(e) => e
+      case _      => And(newExprs)
+    }
+  }
+
 }
+
 
 object Trace {
   def apply(ts: Trees, tt: termination.Trees)(implicit ctx: inox.Context): ExtractionPipeline {
